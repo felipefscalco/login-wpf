cee7ff3ec4344ba6bab1904a29989e797e9954be
Felipe Scalco <felipe@netengine.com.au>
2023-06-30 02:15:02 -0300

    Added EF Core and created initial db

---
+++ Login/Login/Database/Model.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Login.Database
+{
+    public class LoginContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public string DbPath { get; }
+
+        public LoginContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "local.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
---
+++ Login/Login/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Login.Database;
+using System;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Windows;
+
+namespace Login
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using var db = new LoginContext();
+        }
+    }
+}
